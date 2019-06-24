e688d3022acf34be0ef51324c23d591c494c7c74
carrielord <carrie.m.lord@gmail.com>
2019-06-24 16:37:47 -0400

    Added Update/edit view and model

--- RestaurantRaiderMVC/Controllers/RestaurantController.cs
+++ RestaurantRaiderMVC/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRaiderMVC.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -38,6 +39,36 @@ namespace RestaurantRaiderMVC.Controllers
             }
             return View(restaurant);
         }
+
+        //GET: Restaurant/Edit/{id}
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _db.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+        //POST: Restaurant/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(restaurant).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
+
 
         //GET: Restaurant/Delete{id}
         public ActionResult Delete(int? id)
