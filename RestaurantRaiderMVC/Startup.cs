08102930c4e650e664e35c63b04414f8b40d3cfc
carrielord <carrie.m.lord@gmail.com>
2019-06-24 09:57:01 -0400

    Add project files.

---
+++ RestaurantRaiderMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestaurantRaiderMVC.Startup))]
+namespace RestaurantRaiderMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
